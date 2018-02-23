89e5f295977d1986bfd88a26b1532224a4a3c88c
Spencer Russell <spencer.f.russell@gmail.com>
2018-02-23 18:12:56 -0500

    now using external schmitt-trigger oscillator

--- touchsense/touchsense.ino
+++ touchsense/touchsense.ino
@@ -1,45 +1,42 @@
 const int LED_OUT_PIN = 13;
-const int CAP_OUT_PIN = 4;
-const int CAP_IN_PIN = 9;
-long capState = LOW;
-const unsigned long MEASURE_ERROR = 0xffffffff;
+const int OSC_IN_PIN = 2;
+
+volatile unsigned long periodMicros;
+volatile unsigned long lastMicros;
+volatile unsigned int periodCount = 0;
 
 void setup() {
   pinMode(LED_OUT_PIN, OUTPUT);
-  pinMode(CAP_OUT_PIN, OUTPUT);
-  pinMode(CAP_IN_PIN, INPUT);
-  digitalWrite(CAP_OUT_PIN, capState);
+  pinMode(OSC_IN_PIN, INPUT);
   Serial.begin(115200);
-  measureCapMicros(); // do this once so things are settled
+  lastMicros = micros();
+  
+  //attachInterrupt(INT0, oscISR, RISING);
+  
+  // enable INT0 interrupt on change - less overhead than using attachInterrupt()
+  EICRA = 0x03;  // enable INT0 for rising edge
+  EIMSK = 0x01;  // enable INT0
 }
 
 void loop() {
-  unsigned long capTime = measureCapMicros();
-  Serial.println(capTime);
+  Serial.print("{periodMicros: ");
+  Serial.print(periodMicros);
+  Serial.println("}");
+  digitalWrite(LED_OUT_PIN, periodMicros > 200 ? HIGH : LOW);
+  delay(10);
 }
 
-void toggleState() {
-  capState = capState == LOW ? HIGH : LOW;
-}
 
-
-// NB: this isn't the best way to do this if you have other stuff going on - 
-// setting up a timer and interrupt will be much more robust
-unsigned long measureCapMicros() {
-  unsigned long started, stopped;
-  toggleState();
-  started = micros();
-  digitalWrite(CAP_OUT_PIN, capState);
-  while(digitalRead(CAP_IN_PIN) != capState) {
-    // wait for input to settle
-  }
-  stopped = micros();
-  if(stopped - started < MEASURE_ERROR) {
-    return (unsigned int)(stopped - started);
-  }
-  else {
-    // we wrapped around, too lazy to do the right math
-    return MEASURE_ERROR;
+ISR(INT0_vect) {
+  unsigned long now = micros();
+  ++periodCount;
+  if(periodCount == 1) {
+    if(now - lastMicros < 10000) {
+      // otherwise we probably rolled over, so don't update the period
+      periodMicros = now - lastMicros;
+    }
+    lastMicros = now;
+    periodCount = 0;
   }
 }
    
